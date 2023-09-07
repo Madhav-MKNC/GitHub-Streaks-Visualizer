c3c4a144186f156917038c0efe611305f0be5b38
Madhav Kumar <madhavampire@gmail.com>
2023-09-08 00:35:46 +0530

    allcontrifetch

--- graph.py
+++ graph.py
@@ -1,27 +1,38 @@
 import requests
 
-username = 'your_username'
-access_token = 'your_access_token'
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+username = "madhav-mknc"
+access_token = os.getenv("GITHUB_TOKEN")
 
 api_url = 'https://api.github.com/graphql'
 
 query = """
-{
-    user(login: "%s") {
-        contributionsCollection {
-            contributionCalendar {
-                totalContributions
-                weeks {
-                    contributionDays {
-                        date
-                        contributionCount
-                    }
-                }
-            }
+query {
+  user(login: "%s") {
+    login
+    contributionsCollection {
+      contributionCalendar {
+        totalContributions
+
+        weeks {
+          contributionDays {
+            date
+            contributionCount
+          }
         }
+      }
     }
+  }
 }
 """ % username
+
+variables = {
+    'login': username,
+    'after': None
+}
 
 headers = {
     'Authorization': f'Bearer {access_token}',
@@ -29,12 +40,48 @@ headers = {
     'Accept': 'application/json',
 }
 
-response = requests.post(api_url, json={'query': query}, headers=headers)
+all_contribution_data = []
 
-if response.status_code == 200:
-    data = response.json()
-    contribution_days = data['data']['user']['contributionsCollection']['contributionCalendar']['weeks']
-    # Your logic to process contribution_days and visualize the data goes here
-    print(contribution_days)
-else:
-    print(f"Failed to fetch data. Status code: {response.status_code}")
+  
+def fetch():
+  global all_contribution_data
+  global variables
+  
+  while True:
+      response = requests.post(api_url, json={'query': query, 'variables': variables}, headers=headers)
+      data = response.json()
+      print(len(data))
+      # for i in data:
+      #     print(i, data[i])
+
+
+      # if 'errors' in data:
+      #     raise Exception(data['errors'][0]['message'])
+
+      contribution_data = data['data']['user']['contributionsCollection']
+      weeks = contribution_data['contributionCalendar']['weeks']
+
+      for week in weeks:
+         days = week['contributionDays']
+         for day in days:
+            all_contribution_data.append(day)
+
+      if not weeks or len(weeks) < 52:
+          break
+
+      variables['after'] = weeks[-1]['contributionDays'][-1]['date']
+      
+
+
+try:
+   fetch()
+except KeyboardInterrupt:
+   pass
+
+
+# printing
+all_contribution_data = sorted(all_contribution_data, key=lambda x: x['date'])
+
+for i in all_contribution_data:
+   print(f"date: {i['date']}\tcontributions: {i['contributionCount']}")
+   
